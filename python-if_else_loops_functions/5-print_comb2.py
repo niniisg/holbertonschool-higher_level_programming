40a1cab2dee30c7d83fe52971e62bf02c9bc6674
niniisg <nisselsanchez@gmail.com>
2024-05-08 23:43:17 -0400

    fixin number

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-for two_digit in range(99):
+for two_digit in range(100):
 
-    if two_digit == 98:
+    if two_digit == 99:
         print(two_digit)
     else:
         print("{:02d}".format(two_digit), end=', ')
