03889308bf5cb9d2b53c82a9fc966232db0372c6
niniisg <nisselsanchez@gmail.com>
2024-05-08 21:43:38 -0400

    fix variable names

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,12 +7,12 @@ if number >= 0:
     last_d = number % 10
 
 else:
-    last_digit = number % -10
+    last_d = number % -10
 
-if last_digit > 5:
+if last_d > 5:
     print(f"Last digit of {number} is {last_d} and is greater than 5")
 
-elif last_digit == 0:
+elif last_d == 0:
     print(f"Last digit of {number} is {last_d} and is 0")
 else:
     print(f"Last digit of {number} is {last_d} and is less than 6 and not 0")
