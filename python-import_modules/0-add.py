a26064c8eb9c63b6a5252f8beccef26388981c24
niniisg <nisselsanchez@gmail.com>
2024-05-10 14:43:25 -0400

    fix 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-def add(a, b):
-    return a + b
+from add_0 import add
+if __name__ == "__main__":
 
-a = 1
-b = 2
-
-print("{} + {} + = {}".format(a, b, add(a, b)))
+    def main(a, b):
+        return a + b
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
