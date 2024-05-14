20d33c5563f4e643f94d2667cdf76b80304d25c4
niniisg <nisselsanchez@gmail.com>
2024-05-14 18:18:21 -0400

    1-search_replace.py

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+
+    new_list = my_list
+    for item in range(len(new_list)):
+        if new_list[item] == search:
+            new_list[item] = replace
+    return new_list
