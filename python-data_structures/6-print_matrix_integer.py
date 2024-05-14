34870a2db34356c3bf994432727ed82d11d1c8ac
niniisg <nisselsanchez@gmail.com>
2024-05-14 15:37:38 -0400

    0-square_matrix_simple.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for item in row:
-            if item !=0:
+            if item != len(row) - 1:
                 print(" ", end="")
             print("{:d}".format(item), end=" ")
         print()
