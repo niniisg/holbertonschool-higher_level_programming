7e001762ce54a835bad15ae88e30a9872727b334
niniisg <nisselsanchez@gmail.com>
2024-05-15 17:07:21 -0400

    fix 0-safe

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for item in range(x):
             print("{:d}".format(my_list[item]), end="")
-            counter +=1
+            counter += 1
         print()
         return counter
     except IndexError:
