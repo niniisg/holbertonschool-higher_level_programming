61cebc3a4601c416a108f8039e814c78385b4da2
niniisg <nisselsanchez@gmail.com>
2024-05-16 13:53:00 -0400

    0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+this clase contains an empty square
+"""
+
+class Square:
+
+    """
+    A class representing a square
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
+    pass
