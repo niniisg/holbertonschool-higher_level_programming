52a0ff4c08e7952b9eacc8b96858f9fa7688037c
niniisg <nisselsanchez@gmail.com>
2024-05-16 15:25:15 -0400

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+this class contains a define square
+"""
+
+
+class square:
+    """
+    a class to represent a square
+
+    Attributes:
+        the size of Square
+
+    Method:
+        None
+    """
+    def __init__(self, size):
+        self.__size = size
