82ba78a26fdabf172aef747b6d3ac2cda8c13073
niniisg <nisselsanchez@gmail.com>
2024-05-16 17:25:45 -0400

    3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+this class contains a square
+that defines a square.
+"""
+
+
+class Square:
+    """
+    a class to represent a square
+
+    Attributes:
+        __size: the size of square
+    Method:
+        None
+    """
+    def __init__(self, size=0):
+        """
+        this is a constrocture that takes an option
+        argumet name size, and it validates if is an int.
+         """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        return an area of square
+        """
+        area = self.__size * self.__size
+        return area
