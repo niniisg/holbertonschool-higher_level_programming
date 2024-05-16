fabd57efbb7deb75091f87f220e1aa7fdae7d1b7
niniisg <nisselsanchez@gmail.com>
2024-05-16 16:48:02 -0400

    2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+
+"""
+
+
+class Square:
+    """
+    a class to represent a square
+
+    Attributes:
+        __size: the size of square
+    Method:
+        None
+    """
+    def __init__(self, size=0):
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
