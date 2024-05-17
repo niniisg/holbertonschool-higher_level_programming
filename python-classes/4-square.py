451f006c1091b353eba7c7699ebedfaae0ef3e6a
niniisg <nisselsanchez@gmail.com>
2024-05-16 20:03:09 -0400

    4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+this class contains a square
+that defines a square.
+"""
+
+
+class Square:
+    """
+    a class to represent a square
+
+    Attributes:
+        __size: the size of square
+    Method:
+        None
+    """
+    def __init__(self, size=0):
+        """
+        this is a constrocture that takes an option
+        argumet name size, and it validates if is an int.
+         """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        retrives the private property of size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        updates the property of size after
+        validation the new value.
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an int")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        return an area of square
+        """
+        area = self.__size * self.__size
+        return area
