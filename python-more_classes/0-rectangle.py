4f4f1de7fd3b2b1670e9e58d7272037131725e54
niniisg <nisselsanchez@gmail.com>
2024-05-21 14:47:17 -0400

    0-rectangle.py

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+this clase contains an empty Rectangle
+"""
+
+
+class Rectangle:
+    """
+    A class representing a Rectangle
+
+    Attributes:
+        None
+
+    Methods:
+         None
+    """
+    pass
