266906cdfcecddcaaca4e9e7a6eee5c136d39455
niniisg <nisselsanchez@gmail.com>
2024-05-22 20:08:03 -0400

    5-base_geometry.py

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+"""
+this class contain an empty BaseGeometry
+"""
+class BaseGeometry:
+    """
+    a class representing a BaseGeometry
+
+    Atrributes:
+        None:
+
+    Methods:
+        None
+    """
+    pass
