fb95dba7618f4180c3ff985235634dcd58ccc049
niniisg <nisselsanchez@gmail.com>
2024-05-22 23:27:11 -0400

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+this class contains an empty BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    a class representing a BaseGeometry
+
+    Atrributes:
+        None:
+
+    Methods:
+        None
+    """
+    def area(self):
+        """
+        return an area of BaseGeometry
+        """
+        raise Exception("area() is not implemented")
