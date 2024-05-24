baf9a0de0027e89c862a362d5f8250d152ac5517
niniisg <nisselsanchez@gmail.com>
2024-05-24 13:45:02 -0400

    task_00_abc.py

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        """
+        this method shoudl return a string
+        representing the sound made by the animal
+         """
+        pass
+
+
+class Dog(Animal):
+
+    def sound(self):
+        return "Bark"
+
+
+class Cat(Animal):
+    def sound(self):
+        return "meow"
