1b8c9c191fb1ad5771fa46daa11494d932a03861
niniisg <nisselsanchez@gmail.com>
2024-05-24 23:27:48 -0400

    task_03_countediterator.py

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+class CountedIterator:
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.counter = 0
+
+    def __next__(self):
+
+        item = next(self.iterator)
+        self.counter += 1
+        return item
+
+    def get_count(self):
+        return self.counter
