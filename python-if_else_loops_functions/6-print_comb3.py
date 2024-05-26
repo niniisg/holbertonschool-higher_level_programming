177915c549fe11fcaf4bba2f49330248a8970e90
niniisg <nisselsanchez@gmail.com>
2024-05-26 13:30:25 -0400

    fix code

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 
-for i in range(10):
+for i in range(0, 10):
     for number in range(i + 1, 10):
-        print("{}{}, ".format(i, number), end="")
-print("\n")
+        if i == 8 and number == 9:
+            print("{}{}".format(i, number))
+        else:
+            print("{}{}".format(i, number), end=", ")
