e4914820ec4b206e55e8dcc71c7864581c2ab585
niniisg <nisselsanchez@gmail.com>
2024-05-28 18:11:26 -0400

    fix doc

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-
+"""
+module import a JSON object function
+"""
 import json
-"""
-defines a JSON object funtion
-"""
 
 
 def from_json_string(my_str):
     """
     convers a JSON string into a pyhton data structutre
+    Args:
+        my_str (str) JSON string to convert
+    Returns:
+        convert from python object
     """
     return json.loads(my_str)
