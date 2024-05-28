6b14dbc4dc21fede5ad49474ead59c64425aa0c5
niniisg <nisselsanchez@gmail.com>
2024-05-28 17:20:56 -0400

    3-to_json_string.py

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+defines a string funtion to JSON
+"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """
+    returns a JSON representation of a string
+    """
+    return json.dumps(my_obj)
