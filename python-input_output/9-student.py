d11eba068c0471b4e069c4eb4af9834987b2655f
niniisg <nisselsanchez@gmail.com>
2024-05-28 23:54:37 -0400

    9-student.py

---
+++ python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+class Student:
+    """
+    represent a student
+    """
+
+
+def __init__(self, first_name, last_name, age):
+    """
+    initializes the students
+    """
+    self.firts_name = firts_name
+    self.last_name = last_name
+    self.age = age
+
+
+def to_json(self):
+    """
+    returns a dictionary that represent a student
+    """
+    return self.__dict__
