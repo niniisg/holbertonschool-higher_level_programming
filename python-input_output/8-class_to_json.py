ab524ebd413563375b99be290139a9230f5d861b
niniisg <nisselsanchez@gmail.com>
2024-05-28 23:10:39 -0400

    8-class fix pyco

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """
-this module contains a python class-to-JSON fucntion
+this module contains a python class-to-JSON function
 """
+
+
 def class_to_json(obj):
     """
     returns a dictionary represention
