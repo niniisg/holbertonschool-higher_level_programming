a0c8566151f9ced98a4cfaba0d8da22378398f2e
niniisg <nisselsanchez@gmail.com>
2024-05-28 21:45:28 -0400

    6-load_from_json_file.py

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+this module loads data from JSON
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    load data from a JSON file
+
+    Args:
+        filename (Str) the path to JSON file
+    Returns:
+        dict: dictionary loaded from the JSON file
+    """
+    with open(filename, encoding="utf-8") as f:
+          return  json.load(f)
