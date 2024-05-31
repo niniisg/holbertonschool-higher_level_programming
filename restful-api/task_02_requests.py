d24090e666b337d0e15ddb789e97069c083e0adc
niniisg <nisselsanchez@gmail.com>
2024-05-31 18:40:22 -0400

    task2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,24 +1,23 @@
 import requests
 import csv
 
-
 def fetch_and_print_posts():
-    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    print("Status Code: {}".format(response.status_code))
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
-            print('title:{}'.format(post['title']))
-
+            print(post['title'])
 
 def fetch_and_save_posts():
-
-    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
     if response.status_code == 200:
-        post = response.json()
-        data = [{'id': posts['id'], 'title': posts['title'],
-                'body': posts['body']} for posts in post]
+        posts = response.json()
+        data = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
 
-    with open('post.csv', 'w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
-        writer.writeheader()
-        writer.writerows(data)
+        with open('post.csv', 'w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
+            writer.writeheader()
+            writer.writerows(data)
