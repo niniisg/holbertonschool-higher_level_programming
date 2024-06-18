ab0a7c4b381a1ba0516d907e3af14216cf6db5e4
niniisg <nisselsanchez@gmail.com>
2024-06-18 18:33:15 -0400

    7-cities.sql

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+-- writing a script that creates a database hbtn_0d_usa and table cities
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+use hbtn_0d_usa;
+
+CREATE TABlE IF NOT EXISTS cities (
+id INT AUTO_INCREMENT PRIMARY KEY,
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+FOREIGN KEY (state_id) REFERENCES states(id)
+   
+);
