a736750b9d620896477eec3f66212c9c1bbc290f
niniisg <nisselsanchez@gmail.com>
2024-06-18 23:18:50 -0400

    14 fix

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -2,8 +2,8 @@
 
 SELECT tv_genres.name
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = "Dexter"
 GROUP BY name
 ORDER BY tv_generes.name;
