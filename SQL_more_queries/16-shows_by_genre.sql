010e99aafa63e0211b33c9246a2157ea088a712d
niniisg <nisselsanchez@gmail.com>
2024-06-18 23:21:31 -0400

    16-shows_by_genre.sql

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- importing database dump from hbtn_0d_tvshows
+
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+     LEFT JOIN tv_show_genres
+     	  ON tv_show_genres.show_id = tv_shows.id
+     LEFT JOIN tv_genres
+     	  ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name;
