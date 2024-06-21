2fd78d5106d73a848836ca0084d6b09c46c64ed8
niniisg <nisselsanchez@gmail.com>
2024-06-21 12:54:04 -0400

    fixing pyco in model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 """
-This module defines the State class which represents a table structure for 'states' in a database.
-It utilizes SQLAlchemy, an Object-Relational Mapping (ORM) library, to abstract and interact with the database using Python classes and objects.
-    
+This module creates a State class for database
+'states' using SQLAlchemy for ORM.
 classe:
 state: Defines the structure of the 'states' table in the database.
 """
@@ -14,12 +13,13 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """
-    Defines a State class for the 'states' table with 'id' (primary key) 
+    Defines a State class for the 'states' table with 'id' (primary key)
     and 'name' attributes.
     """
-    
+
     __tablename__ = 'states'
     id = Column(Integer, autoincrement=True, unique=True,
                 nullable=False, primary_key=True)
