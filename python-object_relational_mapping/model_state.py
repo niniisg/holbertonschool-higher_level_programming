005e2daa8af3102f3b9a31cad53ebffc794c07b6
niniisg <nisselsanchez@gmail.com>
2024-06-21 12:32:21 -0400

    model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -16,4 +16,4 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, autoincrement=True, unique=True,
                 nullable=False, primary_key=True)
-    name = column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
