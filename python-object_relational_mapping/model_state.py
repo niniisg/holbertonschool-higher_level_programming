63d5379e5e151376405232db58a2c53b2a9a8c4a
niniisg <nisselsanchez@gmail.com>
2024-06-21 12:49:25 -0400

    model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 """
-writing a script that contains the class defenition of
-state and an instance Base = declarative_base()
+This module defines the State class which represents a table structure for 'states' in a database.
+It utilizes SQLAlchemy, an Object-Relational Mapping (ORM) library, to abstract and interact with the database using Python classes and objects.
+    
+classe:
+state: Defines the structure of the 'states' table in the database.
 """
 
 from sqlalchemy import Column, Integer, String
@@ -11,8 +14,12 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-
 class State(Base):
+    """
+    Defines a State class for the 'states' table with 'id' (primary key) 
+    and 'name' attributes.
+    """
+    
     __tablename__ = 'states'
     id = Column(Integer, autoincrement=True, unique=True,
                 nullable=False, primary_key=True)
