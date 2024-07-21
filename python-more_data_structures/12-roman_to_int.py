3f34f9385baa7a8ac6ae482250f56077f72f4bd4
niniisg <nisselsanchez@gmail.com>
2024-07-21 16:02:17 -0400

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -7,6 +7,6 @@ def roman_to_int(roman_string):
     for i in range(len(roman_string)):
         if i > 0 and roman[roman_string[i]] > roman[roman_string[i - 1]]:
             number += roman[roman_string[i]] - 2 * roman[roman_string[i - 1]]
-    else:
-        number += roman[roman_string[i]]
+        else:
+            number += roman[roman_string[i]]
     return number
