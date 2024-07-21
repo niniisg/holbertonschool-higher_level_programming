baf4691764d8c4b067f82b71dc6fbe0c270d8ad9
niniisg <nisselsanchez@gmail.com>
2024-07-21 15:18:39 -0400

    10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     return max(a_dictionary, key=a_dictionary.get)
